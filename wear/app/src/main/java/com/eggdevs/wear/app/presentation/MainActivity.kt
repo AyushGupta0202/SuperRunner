b80acd41ab29293a2a3b212b375be9d444160a02
AyushGupta0202 <ayushg430@gmail.com>
2024-10-20 11:00:54 +0530

    wear os theme setup

--- wear/app/src/main/java/com/eggdevs/wear/app/presentation/MainActivity.kt
+++ wear/app/src/main/java/com/eggdevs/wear/app/presentation/MainActivity.kt
@@ -7,6 +7,7 @@
 package com.eggdevs.wear.app.presentation
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
@@ -14,7 +15,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.wear.compose.material3.Button
 import androidx.wear.compose.material3.Text
+import com.eggdevs.core.presentation.designsystem_wear.SuperRunnerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,11 +26,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(text = "Hello from Wear!")
+            SuperRunnerTheme {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Button(onClick = {
+                        Toast.makeText(this@MainActivity, "toast", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Text(text = "Hello from Wear!")
+                    }
+                }
             }
         }
     }
